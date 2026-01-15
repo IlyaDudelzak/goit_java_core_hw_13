ce6f3b0ef728e559977b4119f300afbf07055c96
Ilya Dudelzak <65915836+IlyaDudelzak@users.noreply.github.com>
2026-01-15 22:33:17 +0200

    Initial commit

---
+++ src/Util.java
@@ -0,0 +1,43 @@
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Files;
+
+abstract public class Util {
+    
+    private static HttpClient client = HttpClient.newBuilder().version(HttpClient.Version.HTTP_1_1).build();
+    private static Gson gson = new Gson();
+    private static String testUrl = "http://jsonplaceholder.typicode.com";
+    public static int maxUserId() throws IOException, URISyntaxException, InterruptedException {
+        HttpResponse<String> response = sendGET(testUrl + "/users");
+
+    }
+    public static HttpResponse<String> sendGET(String Url) throws IOException, InterruptedException, URISyntaxException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(Url))
+                .GET()
+                .headers("Accept", "application/json")
+                .build();
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public static HttpResponse<String> sendPOST(String Url, String jsonPayload) throws IOException, URISyntaxException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(Url))
+                .POST(HttpRequest.BodyPublishers.ofString(jsonPayload))
+                .headers("Accept", "application/json")
+                .build();
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+}
